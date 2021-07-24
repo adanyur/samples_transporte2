4606b767f56cfd5e19e6c1c1d48679327c7a67f8
yur <adanyur@gmail.com>
2021-07-24 14:32:27 -0500

    se mejoro los templates

--- php/contact.php
+++ php/contact.php
@@ -6,34 +6,163 @@
     $cmessage = $_REQUEST['message'];
 	
 	$logo = 'https://'.$_SERVER["HTTP_HOST"].dirname(dirname($_SERVER["REQUEST_URI"])).'/assets/img/hero/h1_hero.jpg';
-    $link = '#';
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' style='width: 100%;' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody>";
-	$body .= "<tr><td colspan='2' style='border:none;'><strong>Asunto:</strong> {$subject}</td></tr>";
-	$body .= "<tr><td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td></tr>";	
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
+	$body='<!DOCTYPE html>
+	<html lang="en">
+	  <head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Document</title>
+		<style type="text/css">
+		  .table__border {
+			/* border-radius: 2rem; */
+			/* border: 1px solid #dadce0; */
+			border: 1px solid #e1ebf7;
+			/* border: 1px solid #f15f22; */
+			width: 50%;
+		  }
+	
+		  .img__ {
+			opacity: 0.5;
+		  }
+		  .mr {
+			margin-right: 2rem;
+		  }
+	
+		  .title__ {
+			text-align: left;
+			color: #2c234d;
+		  }
+	
+		  .title__right {
+			text-align: right;
+			/* color: #64676c; */
+			color: #57667e;
+		  }
+	
+		  .title__,
+		  .title__right {
+			padding: 0.2rem;
+			font-size: 1em;
+			font-family: Arial, Helvetica, sans-serif;
+		  }
+	
+		  .container__table {
+			display: flex;
+			justify-content: space-around;
+		  }
+		  /**/
+		  .img__container {
+			width: 100%;
+			height: 350px;
+		  }
+	
+		  .img__background {
+			background-image: url("https://misionya.com/clients/mrbedoya/catalogo/cargo1/assets/img/hero/h1_hero.jpg");
+			background-size: cover;
+			background-repeat: no-repeat;
+			background-position: center center;
+			height: 100%;
+			width: 100%;
+		  }
+	
+		  .container__text {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-content: space-around;
+			width: 50%;
+			height: 100%;
+		  }
+	
+		  .title__head {
+			margin-bottom: 1.5rem;
+		  }
+	
+		  .title__text,
+		  .label__text,
+		  .text__body,
+		  h1 {
+			color: #fff;
+			font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+		  }
+	
+		  .title__text {
+			font-size: 2rem;
+			text-align: center;
+			color: #f15f22;
+			font-weight: 700;
+		  }
+	
+		  .label__text {
+			font-weight: 600;
+		  }
+	
+		  .text__body {
+			text-align: justify;
+			color: #a4acc3;
+			padding: 0.5rem;
+		  }
+	
+		  .title__coment {
+			/* border: 1px solid red; */
+		  }
+	
+		  .p__0 {
+			padding: 0.5rem 1rem;
+		  }
+		</style>
+	  </head>
+	  <body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+		<table border="0" width="100%" cellpadding="0" cellspacing="0">
+		  <tr>
+			<td height="100"><div class="title__text">Contact Us</div></td>
+		  </tr>
+		  <tr>
+			<td width="500">
+			  <table
+				align="center"
+				cellpadding="0"
+				cellspacing="0"
+				class="table__border"
+			  >
+				<tr>
+				  <td class="container__table">
+					<div class="img__container">
+					  <div class="img__background">
+						<div class="container__text">
+						  <div class="title__head">
+							<div class="label__text p__0">Name: <span>'.$name.'</span></div>
+							<div class="label__text p__0">Email: <span>'.$from.'<span></div>
+						  </div>
+						  <div class="title__coment">
+							<div class="label__text p__0">Comentario</div>
+							<div class="text__body p__0">
+							  '.$cmessage.'
+							</div>
+						  </div>
+						</div>
+					  </div>
+					</div>
+				  </td>
+				</tr>
+			  </table>
+			</td>
+		  </tr>
+		</table>
+	  </body>
+	</html>
+	';
+
 
 
 	$headers = "From: webmaster@example.com" . "\r\n";
 	$headers .= "Reply-To: webmaster@example.com". "\r\n";
+	$headers .= 'Cc: adanyur@gmail.com' . "\r\n";
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1".'X-Mailer: PHP/' . phpversion();
-
-
+	$to = "marcosbedoya@gmail.com";
 	
-
-
-	$to = "adanyur@gmail.com";
-    $send = mail($to, $subject, $body, $headers);
-
-
-
+	$send = mail($to, $subject, $body, $headers);
 
 ?>
--- php/price.php
+++ php/price.php
@@ -14,17 +14,17 @@ $services= isset($_POST['services'])? $_POST['services']:null;
 $imagen= isset($_FILES['imagen'])? $_FILES['imagen']:null;
 $subject = 'Cotizacion';
 
-
-if (isset($_GET['imagen'])){
+$img_url='';
+if (isset($_FILES['imagen'])){
     $sourcePath = $imagen['tmp_name'];
     $targetPath = '../assets/img/upload/'.$imagen['name'];
     move_uploaded_file($sourcePath,$targetPath);
+   $img_url="https://misionya.com/clients/mrbedoya/catalogo/cargo1/assets/img/upload/".$imagen['name'];
+}else{
+
+   $img_url="https://misionya.com/clients/mrbedoya/catalogo/cargo1/assets/img/upload/sin-imagen.jpg";
 }
 
-    $headers = "From: webmaster@example.com" . "\r\n";
-    $headers .= "Reply-To: webmaster@example.com". "\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=ISO-8859-1".'X-Mailer: PHP/' . phpversion();
 
     $body='<!DOCTYPE html>
     <html lang="en">
@@ -32,224 +32,156 @@ if (isset($_GET['imagen'])){
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+        <style type="text/css">
+          .table__border {
+            /* border-radius: 2rem; */
+            /* border: 1px solid #dadce0; */
+            border: 1px solid #e1ebf7;
+            /* border: 1px solid #f15f22; */
+            width: 50%;
+          }
+    
+          .img__ {
+            /*opacity: 0.5;*/
+          }
+          .mr {
+            margin-right: 2rem;
+          }
+    
+          .title__ {
+            text-align: left;
+            color: #2c234d;
+          }
+    
+          .title__right {
+            text-align: right;
+            /* color: #64676c; */
+            color: #57667e;
+          }
+    
+          .title__,
+          .title__right {
+            padding: 0.2rem;
+            font-size: 1.2em;
+            font-family: Arial, Helvetica, sans-serif;
+          }
+    
+          .container__table {
+            display: flex;
+            justify-content: space-around;
+          }
+        </style>
       </head>
-      <body>
-        <div
-          style="
-            border-radius: 2rem;
-            width: 100%;
-            border: 1px solid #dadce0;
-            padding: 2rem 2rem;
-            margin: 1.2rem 0;
-          "
-        >
-          <div
-            style="
-              display: flex;
-              justify-content: space-around;
-              width: 100%;
-              border: 1px solid #bdbdbd;
-              border-radius: 2rem;
-              font-weight: 600;
-              background-color: #ff5f13;
-              color: #fff;
-              margin-bottom: 0.4em;
-            "
-          >
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              name
-            </div>
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              Email
-            </div>
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              Number
-            </div>
-          </div>
-          <div
-            style="
-              display: flex;
-              justify-content: space-around;
-              width: 100%;
-              border: 1px solid #bdbdbd;
-              border-radius: 2rem;
-            "
-          >
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              '{$name}'
-            </div>
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              '{$mail}'
-            </div>
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              '{$number}'
-            </div>
-          </div>
-        </div>
-        <div
-          style="
-            border-radius: 2rem;
-            width: 100%;
-            border: 1px solid #dadce0;
-            padding: 2rem 2rem;
-            margin: 1.2rem 0;
-          "
-        >
-          <div
-            style="
-              display: flex;
-              justify-content: space-around;
-              width: 100%;
-              border: 1px solid #bdbdbd;
-              border-radius: 2rem;
-              font-weight: 600;
-              background-color: #ff5f13;
-              color: #fff;
-              margin-bottom: 0.4em;
-            "
-          >
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              Tipo de Carga
-            </div>
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              Cityo o Department
-            </div>
-          </div>
-          <div
-            style="
-              display: flex;
-              justify-content: space-around;
-              width: 100%;
-              border: 1px solid #bdbdbd;
-              border-radius: 2rem;
-            "
-          >
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-             '{$select}'
-            </div>
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              '{$cityDepartament}'
-            </div>
-          </div>
-        </div>
-        <div
-          style="
-            border-radius: 2rem;
-            width: 100%;
-            border: 1px solid #dadce0;
-            padding: 2rem 2rem;
-            margin: 1.2rem 0;
-          "
-        >
-          <div
-            style="
-              display: flex;
-              justify-content: space-around;
-              width: 100%;
-              border: 1px solid #bdbdbd;
-              border-radius: 2rem;
-              font-weight: 600;
-              background-color: #ff5f13;
-              color: #fff;
-              margin-bottom: 0.4em;
-            "
-          >
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              Contens
-            </div>
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              Weight
-            </div>
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              Height
-            </div>
-          </div>
-          <div
-            style="
-              display: flex;
-              justify-content: space-around;
-              width: 100%;
-              border: 1px solid #bdbdbd;
-              border-radius: 2rem;
-            "
-          >
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              '{$contents}'
-            </div>
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              '{$weight}'
-            </div>
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              '{$height}'
-            </div>
-          </div>
-        </div>
-        <div
-          style="
-            border-radius: 2rem;
-            width: 100%;
-            border: 1px solid #dadce0;
-            padding: 2rem 2rem;
-            margin: 1.2rem 0;
-          "
-        >
-          <div
-            style="
-              display: flex;
-              justify-content: space-around;
-              width: 100%;
-              border: 1px solid #bdbdbd;
-              background-color: #f5f5f5;
-              border-radius: 2rem;
-              font-weight: 600;
-              background-color: #ff5e13;
-              color: #fff;
-              margin-bottom: 0.4em;
-            "
-          >
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              Width
-            </div>
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              Long
-            </div>
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              Extra Service
-            </div>
-          </div>
-          <div
-            style="
-              display: flex;
-              justify-content: space-around;
-              width: 100%;
-              border: 1px solid #bdbdbd;
-              border-radius: 2rem;
-            "
-          >
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              '{$width}'
-            </div>
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              '{$long}'
-            </div>
-            <div style="font-size: 1.3rem; padding: 0.5rem; margin: 0 0 0 0.5rem">
-              '{$services}'
-            </div>
-          </div>
-        </div>
-        <div style="text-align: center">
-          <img
-            src="./assets/img/upload/sin-imagen.jpg"
-            width="500px"
-            height="500px"
-          />
-        </div>
+      <body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+        <table border="0" width="100%" cellpadding="0" cellspacing="0">
+          <tr>
+            <td height="100">
+              <table align="center">
+                <tr>
+                  <th><h1 style="color: #2c234d">Quote</h1></th>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <tr>
+            <td width="500">
+              <table
+                align="center"
+                cellpadding="0"
+                cellspacing="0"
+                class="table__border"
+              >
+                <tr>
+                  <td style="padding: 2rem" class="container__table">
+                    <table border="0" cellpadding="0" cellspacing="0" class="mr">
+                      <tr>
+                        <td>
+                          <img
+                            width="280px"
+                            height="280px"
+                            class="img__"
+                            src="'.$img_url.'"
+                          />
+                        </td>
+                      </tr>
+                    </table>
+                    <table>
+                      <tr>
+                        <td>&nbsp;</td>
+                      </tr>
+                    </table>
+                    <table border="0" cellpadding="0" cellspacing="0">
+                      <tr>
+                        <td>
+                          <table
+                            border="0"
+                            width="100%"
+                            cellpadding="0"
+                            cellspacing="0"
+                          >
+                            <tr>
+                              <th class="title__">Name</th>
+                              <td class="title__right">'.$name.'</td>
+                            </tr>
+                            <tr>
+                              <th class="title__">Email</th>
+                              <td class="title__right">'.$mail.'</td>
+                            </tr>
+                            <tr>
+                              <th class="title__">Number</th>
+                              <td class="title__right">'.$number.'</td>
+                            </tr>
+                            <tr>
+                              <th class="title__">City</th>
+                              <td class="title__right">'.$cityDepartament.'</td>
+                            </tr>
+                            <tr>
+                              <td colspan="2">&nbsp;</td>
+                            </tr>
+                            <tr>
+                              <th class="title__">Contens</th>
+                              <td class="title__right">'.$contents.'</td>
+                            </tr>
+                            <tr>
+                              <th class="title__">Weight</th>
+                              <td class="title__right">'.$weight.'</td>
+                            </tr>
+                            <tr>
+                              <th class="title__">Height</th>
+                              <td class="title__right">'.$height.'</td>
+                            </tr>
+                            <tr>
+                              <th class="title__">Width</th>
+                              <td class="title__right">'.$width.'</td>
+                            </tr>
+                            <tr>
+                              <th class="title__">Long</th>
+                              <td class="title__right">'.$long.'</td>
+                            </tr>
+                            <tr>
+                            <th class="title__">Extra Service</th>
+                            <td class="title__right">'.$services.'</td>
+                          </tr>
+                          </table>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
       </body>
-    </html>
-    ';
+    </html>';
+  
+  $headers = "From: webmaster@example.com" . "\r\n";
+	$headers .= "Reply-To: webmaster@example.com". "\r\n";
+	$headers .= 'Cc: adanyur@gmail.com' . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1".'X-Mailer: PHP/' . phpversion();
+	$to = "marcosbedoya@gmail.com";
 
-
-    $to = "adanyur@gmail.com";
     $send = mail($to, $subject, $body, $headers);
-
 ?>
